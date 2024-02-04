4aefd5b3074fbad2d5d592d72faeebed908adc01
prashant <prashanttalele1997@gmail.com>
2024-02-04 22:06:45 +0530

    commit

---
+++ 02_Simle_Node_Learning_project/04_Asyncrons.js
@@ -0,0 +1,15 @@
+
+console.log("***********First***********")
+console.log("Car Start");
+console.log("Car Slow");
+console.log("Car Fast");
+
+
+
+console.log("***********Second***********")
+
+console.log("Car Start");
+console.log("Car Fast");
+setTimeout(()=>{
+     console.log("Car Slow");
+},2000)
