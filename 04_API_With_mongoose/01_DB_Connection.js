3f610a52b5b114a8bdaa799e80d17d5f861de295
prashant <prashanttalele1997@gmail.com>
2024-02-09 11:56:01 +0530

    commit

---
+++ 04_API_With_mongoose/01_DB_Connection.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+const main = async ()=> {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/prashant');
+        const farmerSchema = new mongoose.Schema({
+            name: String
+        });
+        const farmerModel = mongoose.model('Farmer',farmerSchema);
+        let data = new farmerModel({ name: "Pratik"});
+        let result = await data.save();    // Wait for the promise to resolve
+         console.log(result);
+        } catch (error) {
+          console.error(error);
+    }   
+    }  
+
+main()
+
+
