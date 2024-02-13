7ab9e069b53d4224ef8c7ff7a95d5bbb0a102368
prashant <prashanttalele1997@gmail.com>
2024-02-13 20:38:53 +0530

    inital commit

---
+++ 05_MVC_Model_View_Controller/Routes/Product.js
@@ -0,0 +1,11 @@
+c
+const express=require('express')
+const route=express.Router()
+
+route.routes("/").get(getAllProduct);
+route.routes("/testing").get(getAllProductTesting)
+
+
+module.exports=route
+
+
